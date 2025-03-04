9bc0a3c995be2a18c86832e486ede17e2a259616
kicet3 <kicet3@gmail.com>
2025-03-04 10:40:33 +0900

    불필요한 import 제거

--- backend/danawaCrawl.py
+++ backend/danawaCrawl.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-import time
 import re
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
